7c9505a26cffedc5f9012527190d1d0853de4e0f
Chang <234737041@qq.com>
2022-08-17 16:55:49 +0800

    add reg.ejs

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+//数据库
+var MongoStore=require('connect-mongo');
+var settings = require('../settings');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+//路由
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -12,7 +15,6 @@ var expressLayouts = require('express-ejs-layouts');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 app.set('view options',{layout:true});
 
 app.use(expressLayouts);
@@ -20,7 +22,15 @@ app.use(expressLayouts);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.set(express.bodyParser());
+app.set(express.methodOveride());
 app.use(cookieParser());
+app.use(express.session({
+  secret:settings.cookieParser,
+  store:new MongoStore({
+    db:settings.db
+  })
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 //jquery
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
